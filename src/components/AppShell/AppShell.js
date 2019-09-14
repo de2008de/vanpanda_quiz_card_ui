efd2a1318e7d0dff62e3c0b2e9a815273675a1ec
de2008de <mrycchou@gmail.com>
2019-09-14 15:52:07 -0700

    Add AppBar

---
+++ src/components/AppShell/AppShell.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WCAppBar from './WCAppBar';
+
+const AppShell = () => {
+    return (
+        <div className="AppShell">
+            <WCAppBar />
+        </div>
+    );
+}
+
+export default AppShell;
