98daf22d271f68edf4073950c837ccb7d3b59b38
de2008de <mrycchou@gmail.com>
2019-09-21 20:53:54 -0700

    Remove KeyPoints and use content instead

--- src/components/commons/BookmarkPage.js
+++ src/components/commons/BookmarkPage.js
@@ -1,13 +1,29 @@
 import React from "react";
 import DetailCard from "../Card/DetailCard";
 
+// Mock up data
+const content =
+    "Demand refers to consumers' desire to purchase goods and services at given prices.\n Demand refers to consumers' desire to purchase goods and services at given prices. ";
+
 const BookmarkPage = () => {
     return (
         <div className="BookmarkPage">
-            <DetailCard />
-            <DetailCard />
-            <DetailCard />
-            <DetailCard />
+            <DetailCard
+                title={"What is Demand definition?"}
+                content={content}
+            />
+            <DetailCard
+                title={"What is Demand definition?"}
+                content={content}
+            />
+            <DetailCard
+                title={"What is Demand definition?"}
+                content={content}
+            />
+            <DetailCard
+                title={"What is Demand definition?"}
+                content={content}
+            />
         </div>
     );
 };
