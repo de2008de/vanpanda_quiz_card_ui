f0e5b1826e0f814678ce9594a0d67423a16f7fcb
de2008de <mrycchou@gmail.com>
2019-09-21 21:38:16 -0700

    Add logic to determine environment and server ip

--- src/configs/ServerConfig.js
+++ src/configs/ServerConfig.js
@@ -1,7 +1,20 @@
-const oServerConfig = {
-    api: {
-        ip: "http://localhost:8080"
+const determineEnvironment = () => {
+    const currentHost = window.location.host;
+    let ip;
+    if (currentHost.indexOf("localhost") !== -1) {
+        // develop
+        ip = "http://localhost:8080"
+    } else {
+        // production
+        ip = "https://gentle-peak-28389.herokuapp.com"
     }
-}
+    return {
+        api: {
+            ip: ip
+        }
+    };
+};
+
+const oServerConfig = determineEnvironment();
 
 export default oServerConfig;
