35c4e675d68c02c8adba8da96ff31f96d5609d21
de2008de <mrycchou@gmail.com>
2019-10-26 00:23:50 -0700

    Add water wave circle to StudyPageResult + Create a WaterWaveCircle component

--- src/components/commons/StudyPageResult.js
+++ src/components/commons/StudyPageResult.js
@@ -1,10 +1,28 @@
 import React from "react";
 import qs from "query-string";
 import DetailCard from "../Card/DetailCard";
+import Typography from '@material-ui/core/Typography';
+import WaterWaveCircle from "../indicator/WaterWaveCircle";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+    waterWaveContainer: {
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        margin: "1rem"
+    },
+    scoreText: {
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center"
+    }
+}));
 
 const StudyPageResult = props => {
+    const classes = useStyles();
     const result = JSON.parse(qs.parse(props.location.search).result);
-    
+
     const getTotalScore = result => {
         const totalConceptCards = result.masteredConceptCard.length + result.needImprovementConceptCard.length;
         const numMasteredConceptCard = result.masteredConceptCard.length;
@@ -12,9 +30,20 @@ const StudyPageResult = props => {
         return score;
     }
 
+    const score = getTotalScore(result);
+
     return (
         <div className="StudyPageResult">
-            Your score is { getTotalScore(result) }%
+            <div className={classes.waterWaveContainer}>
+                <WaterWaveCircle
+                    percent={score}
+                />
+            </div>
+            <div className={classes.scoreText}>
+                <Typography variant="h6">
+                    Your score is {score}%
+                </Typography>
+            </div>
         </div>
     );
 };
