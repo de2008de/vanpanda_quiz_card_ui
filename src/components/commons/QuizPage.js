a72f200d48ee1fee115d2e9d9ea76b90b66eec65
de2008de <mrycchou@gmail.com>
2019-10-28 23:52:19 -0700

    Add bookmark state to detail card on QuizPage

--- src/components/commons/QuizPage.js
+++ src/components/commons/QuizPage.js
@@ -4,12 +4,14 @@ import axios from "axios";
 import qs from "query-string";
 import ServerConfig from "../../configs/ServerConfig";
 import DetailCard from "../Card/DetailCard";
+import { getBookmarks, convertBookmarkArrayToMap } from "../api/BookmarkApiHelper";
 
 const sStudyCardApi = "/api/v1/card/studycard";
 
 const QuizPage = props => {
     const appContext = useContext(AppContext);
     const [studyCard, setStudyCard] = useState({});
+    const [bookmarks, setBookmarks] = useState({});
     const [indexOfQuestion, setIndexOfQuestion] = useState(0);
     const studyCardId = qs.parse(props.location.search).id;
 
@@ -28,6 +30,36 @@ const QuizPage = props => {
             .catch(() => { });
     }, [appContext, studyCardId]);
 
+    useEffect(() => {
+        // If we have cache, then use cache
+        if (appContext && appContext.bookmarks) {
+            setBookmarks(appContext.bookmarks);
+            return;
+        }
+        getBookmarks()
+            .then(response => {
+                const bookmarks = response.data.data.bookmarks;
+                const bookmarkMap = convertBookmarkArrayToMap(bookmarks);
+                setBookmarks(bookmarkMap);
+            })
+            .catch(() => { });
+    }, [appContext]);
+
+    const boomarkOnClickCallback = conceptCardId => {
+        return () => {
+            setBookmarks(bookmarks => {
+                if (bookmarks[conceptCardId]) {
+                    delete bookmarks[conceptCardId];
+                } else {
+                    bookmarks[conceptCardId] = {
+                        conceptCardId: conceptCardId
+                    }
+                }
+                return bookmarks;
+            });
+        }
+    };
+
     const showQuizQuestion = () => {
         if (!studyCard.conceptCards) {
             return false;
@@ -35,12 +67,14 @@ const QuizPage = props => {
         const conceptCard = studyCard.conceptCards[indexOfQuestion];
         const conceptCardId = conceptCard.id;
         const definition = conceptCard.definition;
-
+        const isBookmared = bookmarks[conceptCardId];
         return (
             <DetailCard
                 id={conceptCardId}
                 key={conceptCardId}
                 definition={definition}
+                bookmarked={isBookmared}
+                boomarkOnClickCallback={boomarkOnClickCallback(conceptCardId)}
             >
             </DetailCard>
         );
