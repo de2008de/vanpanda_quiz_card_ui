e66b266c0d62013e07eb4525fa122257b00e389e
de2008de <mrycchou@gmail.com>
2019-11-03 05:45:10 -0800

    Building SearchBar + Add styles to SearchBar

--- src/components/explore/ExplorePage.js
+++ src/components/explore/ExplorePage.js
@@ -1,10 +1,21 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+    searchBarWrapper: {
+        width: "70%",
+        margin: "0.5rem 0.5rem 1rem 1rem"
+    }
+}));
 
 const ExplorePage = props => {
+    const classes = useStyles();
     return (
         <div className="ExplorePage">
-            <SearchBar />
+            <div className={classes.searchBarWrapper}>
+                <SearchBar />
+            </div>
         </div>
     );
 };
