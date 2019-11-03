98f64252b5aef960ba83cbf72ceb2609eccb6647
de2008de <mrycchou@gmail.com>
2019-11-02 21:06:52 -0700

    Add empty SearchBar

--- src/components/explore/ExplorePage.js
+++ src/components/explore/ExplorePage.js
@@ -1,8 +1,10 @@
 import React from "react";
+import SearchBar from "./SearchBar";
 
 const ExplorePage = props => {
     return (
         <div className="ExplorePage">
+            <SearchBar />
         </div>
     );
 };
