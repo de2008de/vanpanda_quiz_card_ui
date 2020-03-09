47ee2095c9d61055fcb76a028f4ba0064c452e15
David Chou <mrycchou@gmail.com>
2020-03-08 22:47:43 -0700

    Update my study card page UI

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -1,17 +1,14 @@
 import '../../assets/css/Home/HomePage.css'
 
-import Box from '@material-ui/core/Box'
 import Button from '@material-ui/core/Button'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import Tab from '@material-ui/core/Tab'
 import Tabs from '@material-ui/core/Tabs'
-import Typography from '@material-ui/core/Typography'
 import React, { useEffect, useState } from 'react'
 import { makeStyles } from '@material-ui/core'
 import { CancelToken, CancelTokenSource } from 'axios'
 import { History, LocationState } from 'history'
 
-import vanpandaLogo from '../../assets/svg/vanpanda_logo.svg'
 import { StudyCard } from '../../types/cards'
 import { getAxioCancelTokenSource } from '../../helpers/general'
 import { doAuthentication } from '../../utils/auth'
@@ -22,13 +19,18 @@ interface Props {
 };
 
 const useStyles = makeStyles(theme => ({
-    headerContainer: {
-        display: "flex",
-        flexWrap: "wrap",
-        margin: "1rem 1rem 0.5rem 1rem"
+    headerBackground: {
+        color: "#fff",
+        background: "linear-gradient(to left, #1d77d1 0%, #5ca9f7 74%)",
+        padding: "1rem 0 1rem 0",
+        boxShadow: "0px 5px 10px 0px #8fc7ff"
     },
-    logo: {
-        width: "6rem"
+    title: {
+        textAlign: "center",
+        fontSize: "3rem",
+        marginBottom: "2rem",
+        fontFamily: "Schoolbell",
+        opacity: 0.87
     },
     loadMoreWrapper: {
         display: "flex",
@@ -160,44 +162,41 @@ const HomePage = (props: Props) => {
 
     return (
         <div className="HomePage">
-            <div className={classes.headerContainer}>
+            <div className={classes.headerBackground}>
+
+                <div className={classes.title}>
+                    My Study Cards
+            </div>
+
                 <div>
-                    <Typography variant="h5">
-                        <Box fontWeight="bold">My</Box>
-                    </Typography>
-                    <Typography variant="h5">
-                        <Box>Study Cards</Box>
-                    </Typography>
+                    <Tabs
+                        value={tabValue}
+                        onChange={onTabChangeHandler}
+                        indicatorColor="primary"
+                        textColor="#fff"
+                        centered
+                    >
+
+                        <Tab label="All" value="all" />
+                        <Tab label="created By Me" value="createdByMe" />
+
+                    </Tabs>
                 </div>
-                <div style={{ flexGrow: 1 }}></div>
-                <div>
-                    <img src={vanpandaLogo} className={classes.logo} alt="vanpanda_logo" />
+
+            </div>
+
+            <div className="cards">
+
+                <div className="content">
+                    {renderStudyCards(studyCards, true, props.history)}
                 </div>
-            </div>
+                <div className={classes.loadMoreWrapper}>
+                    {renderLoadMoreButton()}
+                </div>
+                <div className={classes.circularProgressWrapper}>
+                    {renderLoadingCircularProgress()}
+                </div>
 
-            <div>
-                <Tabs
-                    value={tabValue}
-                    onChange={onTabChangeHandler}
-                    indicatorColor="primary"
-                    textColor="primary"
-                    centered
-                >
-
-                    <Tab label="All" value="all" />
-                    <Tab label="created By Me" value="createdByMe" />
-
-                </Tabs>
-            </div>
-
-            <div className="content">
-                {renderStudyCards(studyCards, true, props.history)}
-            </div>
-            <div className={classes.loadMoreWrapper}>
-                {renderLoadMoreButton()}
-            </div>
-            <div className={classes.circularProgressWrapper}>
-                {renderLoadingCircularProgress()}
             </div>
         </div>
     );
